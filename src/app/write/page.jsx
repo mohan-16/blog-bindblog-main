49daffdab43f89bacf1cc3609c56f1e81ed56534
mohan-16 <mohanrajg1630@gmail.com>
2024-03-13 13:16:32 +0530

    updated login page

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,12 +1,20 @@
 "use client";
+import { useState, lazy, Suspense, useEffect } from "react";
 import Image from "next/image";
 import styles from "./writePage.module.css";
-import { useState } from "react";
-import ReactQuill from "react-quill";
+
+const DynamicReactQuill = lazy(() => import("react-quill"));
 import "react-quill/dist/quill.bubble.css";
+
 const WritePage = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
   return (
     <div className={styles.container}>
       <input type="text" placeholder="Title" className={styles.input} />
@@ -28,13 +36,18 @@ const WritePage = () => {
           </div>
         )}
 
-        <ReactQuill
-          className={styles.textArea}
-          theme="bubble"
-          value={value}
-          onChange={setValue}
-          placeholder="Just Blog your story...!!"
-        />
+        {/* Render ReactQuill only in the browser */}
+        {isBrowser && (
+          <Suspense fallback={<div>Loading...</div>}>
+            <DynamicReactQuill
+              className={styles.textArea}
+              theme="bubble"
+              value={value}
+              onChange={setValue}
+              placeholder="Just Blog your story...!!"
+            />
+          </Suspense>
+        )}
       </div>
       <button className={styles.publish}>Publish</button>{" "}
     </div>
